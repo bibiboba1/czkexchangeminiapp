838230de0c06ca1cf4a697b1277d6425ea5c78cf
bibiboba1 <dmitrijhhh18@gmail.com>
2025-08-08 17:46:57 +0200

    Update second.js

--- second.js
+++ second.js
@@ -1,11 +1,14 @@
-document.getElementById('toAccount').addEventListener('click', () => {
-      window.location.href = 'account.html';
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('toAccount')?.addEventListener('click', () => {
+    window.location.href = 'account.html';
+  });
 
-    // Клик по "Назад"
-    document.getElementById('goBack').addEventListener('click', () => {
-      window.location.href = 'index.html';
-    });
-document.getElementById('toCash').addEventListener('click', () => {
-  window.location.href = 'cash.html';
+  document.getElementById('toCash')?.addEventListener('click', () => {
+    window.location.href = 'cash.html';
+  });
+
+  document.getElementById('goBack')?.addEventListener('click', () => {
+    window.location.href = 'index.html';
+  });
 });
+
