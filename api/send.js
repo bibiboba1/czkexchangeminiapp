720fbecb1009ef2368a3a5865d34d136fec29e8d
bibiboba1 <dmitrijhhh18@gmail.com>
2025-08-08 21:33:10 +0200

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,5 +1,3 @@
-// api/send.js
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
@@ -46,3 +44,4 @@ export default async function handler(req, res) {
     res.status(500).json({ error: 'Server error', details: err.message });
   }
 }
+
