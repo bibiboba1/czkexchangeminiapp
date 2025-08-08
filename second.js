51dea915c43d8fa697a6690843b3d5cc2026ed5a
bibiboba1 <dmitrijhhh18@gmail.com>
2025-08-08 15:04:21 +0200

    Create second.js

---
+++ second.js
@@ -0,0 +1,11 @@
+document.getElementById('toAccount')?.addEventListener('click', () => {
+  window.location.href = 'account.html';
+});
+
+document.getElementById('toCash')?.addEventListener('click', () => {
+  window.location.href = 'cash.html';
+});
+
+document.getElementById('goBack')?.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
