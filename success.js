902f0bb215d129ead92c17903d8af2a406c52f16
bibiboba1 <dmitrijhhh18@gmail.com>
2025-08-10 16:56:21 +0200

    Update success.js

--- success.js
+++ success.js
@@ -7,12 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const encodedMessage = encodeURIComponent(message);
 
     // Ссылка на чат с ботом и готовым текстом
-    const chatUrl = `https://t.me/innnntro_bot?text=${encodedMessage}`;
+    const chatUrl = `https://t.me/big_whipper?text=${encodedMessage}`;
 
     // Открываем чат
     window.open(chatUrl, '_blank');
   });
 });
-
-
 
